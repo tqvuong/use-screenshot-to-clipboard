0dcea3b4c962b164049071e64944fe54d8153c11
tqvuong <tqvuong.dev@gmail.com>
2023-06-23 22:01:41 +0700

    Add example and update README.md

---
+++ example/build/precache-manifest.e8dd55707e76f86fce3cc4c8461890a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513bce2c14b5cfdef6735702eb02178a",
+    "url": "/use-screenshot-to-clipboard/index.html"
+  },
+  {
+    "revision": "2bfec3a94a22b463b2bc",
+    "url": "/use-screenshot-to-clipboard/static/css/main.b69d7019.chunk.css"
+  },
+  {
+    "revision": "b5e53a9fe3ac3c5ff5f6",
+    "url": "/use-screenshot-to-clipboard/static/js/2.8291da7b.chunk.js"
+  },
+  {
+    "revision": "e59cfee9cb924b3b76073a98687bacae",
+    "url": "/use-screenshot-to-clipboard/static/js/2.8291da7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bfec3a94a22b463b2bc",
+    "url": "/use-screenshot-to-clipboard/static/js/main.807f652c.chunk.js"
+  },
+  {
+    "revision": "4011fe9b68114e901449",
+    "url": "/use-screenshot-to-clipboard/static/js/runtime-main.d765b4cd.js"
+  }
+]);
